74cee935977870055ef341d510b1e9d2148ad261
itstudent17 <itstudent17@outlook.com>
2021-02-02 16:50:39 +0300

    state added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { TodoListItem } from "./components/TodoListItem";
 
-const todos: Array<Todo> = [
+const initialTodos: Array<Todo> = [
   { text: "Walk the dog", isCompleted: true },
   { text: "Create app", isCompleted: false },
 ];
 
-const App: React.FC = () => (
-  <div>
+const App: React.FC = () => {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo: ToggleTodo = (selectedTodo) => {
+    // не [...todos]
+    const newTodos = todos.map((todo) =>
+      // почему сравниваются 2 объекта напрямую, а не id
+      selectedTodo === todo ? { ...todo, isCompleted: !todo.isCompleted } : todo
+    );
+
+    setTodos(newTodos);
+  };
+
+  return (
     <>
-      {todos.map((todo) => (
-        <TodoListItem todo={todo} />
+      {todos.map((todo: Todo) => (
+        <TodoListItem todo={todo} toggleTodo={toggleTodo} />
       ))}
     </>
-  </div>
-);
+  );
+};
 
 export default App;
