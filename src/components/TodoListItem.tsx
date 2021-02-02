f189ad281994fa66866f49331d44d46e7f48e9f3
itstudent17 <itstudent17@outlook.com>
2021-02-02 15:50:57 +0300

    refactoring and styling

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -1,14 +1,16 @@
+import "./TodoListItem.css";
+
 interface TodoListItemProps {
-  todo: {
-    text: string;
-    isCompleted: boolean;
-  };
+  todo: Todo;
 }
 
 export const TodoListItem: React.FC<TodoListItemProps> = ({ todo }) => {
   return (
     <li>
-      <label htmlFor="">
+      <label
+        // style={{ textDecoration: todo.isCompleted ? "line-through" : "none" }}
+        className={todo.isCompleted ? "complete" : undefined}
+      >
         <input type="checkbox" checked={todo.isCompleted} />
         {todo.text}
       </label>
