44c6399c751bc49d6bb92ef368b19213c5936f7b
itstudent17 <itstudent17@outlook.com>
2021-02-02 16:50:19 +0300

    onChange handler

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -2,16 +2,24 @@ import "./TodoListItem.css";
 
 interface TodoListItemProps {
   todo: Todo;
+  toggleTodo: ToggleTodo;
 }
 
-export const TodoListItem: React.FC<TodoListItemProps> = ({ todo }) => {
+export const TodoListItem: React.FC<TodoListItemProps> = ({
+  todo,
+  toggleTodo,
+}) => {
   return (
     <li>
       <label
         // style={{ textDecoration: todo.isCompleted ? "line-through" : "none" }}
         className={todo.isCompleted ? "complete" : undefined}
       >
-        <input type="checkbox" checked={todo.isCompleted} />
+        <input
+          type="checkbox"
+          checked={todo.isCompleted}
+          onChange={() => toggleTodo(todo)}
+        />
         {todo.text}
       </label>
     </li>
