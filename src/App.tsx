5193e18a99315ec39df6530f25cd5ad7d3fb46d4
itstudent17 <itstudent17@outlook.com>
2021-02-02 15:51:24 +0300

    mapping added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { TodoListItem } from "./components/TodoListItem";
 
-const App: React.FC = () => <div>Hello world!</div>;
+const todos: Array<Todo> = [
+  { text: "Walk the dog", isCompleted: true },
+  { text: "Create app", isCompleted: false },
+];
+
+const App: React.FC = () => (
+  <div>
+    <>
+      {todos.map((todo) => (
+        <TodoListItem todo={todo} />
+      ))}
+    </>
+  </div>
+);
 
 export default App;
